d7e5ff4c6ce6c23aa743a44a56b8523d979ebd04
user <alexandros.tsakiridis2@gmail.com>
2023-04-10 19:41:01 +0300

    edit

--- coupons/assets.py
+++ coupons/assets.py
@@ -2,15 +2,16 @@ from random import choice, randint
 
 _CODE = str
 
-class Generator:
 
+class Generator:
     _LETTERS: list[str] = [chr(position) for position in range(65, 91)]  # - get latin capital letters from ascii chart
 
     SAMPLE: list[_CODE] = ["WELC-OMET-OTHE", "IEEE-XTRE-ME14", "AAAA-0000-A0A0",
                            "AAAA-0000-A0A1", "AAAA-0000-A0AB", "AAAA-0000-ABAB"]
 
     @staticmethod
-    def load(file_name: str) -> list[_CODE]: return open(file_name, "r").readlines()  # - read pre-generated codes file
+    def load(file_name: str) -> list[_CODE]:
+        return open(file_name, "r").readlines()  # - read pre-generated codes file
 
     @staticmethod
     def _code() -> list[str, str, str]:  # ------------------ protected method generating a list of three code segments
@@ -19,19 +20,24 @@ class Generator:
         return out + [str().join([choice(Generator._LETTERS) for _ in range(4)])]
 
     @staticmethod
-    def code() -> _CODE: return str().join(Generator._code())  # ------------------- version without readability dashes
+    def code() -> _CODE:
+        return str().join(Generator._code())  # ------------------- version without readability dashes
 
     @staticmethod
-    def dash() -> _CODE: return str("-").join(Generator._code())  # ------------------- version with readability dashes
+    def dash() -> _CODE:
+        return str("-").join(Generator._code())  # ------------------- version with readability dashes
 
     @staticmethod
     def alter(code: _CODE) -> _CODE:  # --------------------------- remove dashes if they exist, add them if they don't
         assert len(code) in (12, 14)
-        if len(code) == 12: code = code[:4] + "-" + code[4:8] + "-" + code[8:]
-        elif len(code) == 14: code = code.replace("-", str())
+        if len(code) == 12:
+            code = code[:4] + "-" + code[4:8] + "-" + code[8:]
+        elif len(code) == 14:
+            code = code.replace("-", str())
         return code
 
-    def __call__(self, amount: int) -> list[_CODE]: return [self.dash() for _ in range(amount)]  # ----- specify amount
+    def __call__(self, amount: int) -> list[_CODE]:
+        return [self.dash() for _ in range(amount)]  # ----- specify amount
 
 
 class Hamming:
@@ -53,8 +59,10 @@ class Hamming:
         flag: bool = False
         for left_char, right_char in zip(left, right):
             if left_char != right_char:
-                if flag: return False
-                else: flag = True
+                if flag:
+                    return False
+                else:
+                    flag = True
         return True
 
     def test(self, data: list[_CODE]):
